894c5e4959b82c1235ef55f2a03af512ee93197c
Annie Wu <anniewu@Annies-MacBook-Pro.local>
2023-12-02 18:24:42 -0500

    lint

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from dotenv import load_dotenv
 import os
-import requests
-import json
-import base64
+
+# import requests
+# import json
+# import base64
 from flask import (
     Flask,
     redirect,
     render_template,
     request,
     url_for,
-    jsonify,
+    # jsonify,
 )
 import openai
 
@@ -33,7 +34,8 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
 
     1 - I will give an illness I have
     2 - Provide the list of nutritions I need to take
-    3 - Give only the list of the nutritions, without the food, description, or function of the nutrition
+    3 - Give only the list of the nutritions, without the food, 
+    description, or function of the nutrition
     4 - Do not include medical disclaimer
     5 - For the nutritrion part, I want it in this format:
 
